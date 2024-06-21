5b3c61e90b71adf821a250c29baebe0c5b19f4b2
matheus.domeneghetti <matheus.domeneghetti@RPOLOG0046>
2024-06-21 15:50:20 -0300

    Adicionar arquivos de projeto.

---
+++ Estudante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExercicioVetor
+{
+    class Estudante
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return "Quartos ocupados: "
+            + Nome + ", "
+            + Email;
+        }
+    }
+}
